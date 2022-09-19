ff02fde80241a1217ca1dfb3273be60546c9df6e
Yoon-TG <witteny@gamil.com>
2022-09-19 16:01:55 +0900

    navbar 1차 완성

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import './App.css';
 
 function Main() {
   return (
-    <div class="navbar">
+    <div>
         <nav class="navbar navbar-expand-lg bg-light">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
-          <img src={process.env.PUBLIC_URL+'/test.jpg'}></img>
+          <img src={process.env.PUBLIC_URL+'/images/test.jpg'} style={{width:80}}></img>
         </a>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav">
             <a class="nav-link active" aria-current="page" href="#">미션</a>
             <a class="nav-link" href="#">이용방법</a>
             <a class="nav-link" href="#">못난이상점</a>
-            <a class="nav-link disabled">레시피</a>
+            <a class="nav-link" href="#">레시피</a>
+          </div>
+          <div class="rightsideHeader">
+            <img src={process.env.PUBLIC_URL+'/images/cart.png'} style={{width:20}}></img>
+            <img src={process.env.PUBLIC_URL+'/images/person.png'} style={{width:25}}></img>
           </div>
         </div>
       </div>
-    </nav>
+      </nav>
     </div>
   );
 }

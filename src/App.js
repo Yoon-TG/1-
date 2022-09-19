e45da7197a382c2dd015618339c1364a8938d9e4
Yoon-TG <witteny@gamil.com>
2022-09-19 17:31:10 +0900

    middleBanner 작업중(텍스트 css 스타일 바꾸기 ing)

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ function Main() {
                 <a class="nav-link" href="#">레시피</a>
               </div>
               <div class="rightsideHeader">
-                <img src={process.env.PUBLIC_URL+'/images/cart.png'} style={{width:20}}></img>
-                <img src={process.env.PUBLIC_URL+'/images/person.png'} style={{width:25}}></img>
+                <img src={process.env.PUBLIC_URL+'/images/cart.png'} style={{width:20}} alt='<a href="https://www.flaticon.com/kr/free-icons/" title="카트 아이콘">카트 아이콘  제작자: uicon - Flaticon</a>'></img>
+                <img src={process.env.PUBLIC_URL+'/images/person.png'} style={{width:25}} alt='<a href="https://www.flaticon.com/kr/free-icons/" title="사람 아이콘">사람 아이콘  제작자: spaceman.design - Flaticon</a>'></img>
               </div>
             </div>
           </div>
@@ -33,12 +33,29 @@ function Main() {
           </span>
         </div>
 
-        <div>
-          {/* 이미지 파일 추가하기 */}
+        <div className='middleBanner'>  
+          <img style={{width: 'auto', height:'130px', textAlign: 'center'}} src='https://ik.imagekit.io/uhpparxr6/main/main_vp/main_vp_pc_cBSZrC968.png?ik-sdk-version=javascript-1.4.3&updatedAt=1655786748538&ik-sdk-version=react-1.1.1'></img>
+          <br />
+          <br />
+          <br />
+          <div>
+            <span className='leftText'>이렇게 많은 분들이</span>
+            <span className='rightText'>전체 만족도   </span>
+            <span className='rightText'>   전체 리뷰 수</span>
+            <br />
+            <span className='leftText'>지속 가능한 식탁을 만들고 있어요!</span>
+            <span className='rightText'>4.8       </span>
+            <span className='rightText'>7257</span>
+          </div>
+
         </div>
 
-        <div>
-          {/* 지속 가능한 식탁~ */}
+        <div >
+          <div>
+            
+          </div>
+          
+          
         </div>
     </div>
   );

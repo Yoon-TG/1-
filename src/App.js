52b2c05f37013d424dc3f40da6bd130801a45862
Yoon-TG <witteny@gamil.com>
2022-09-19 15:29:52 +0900

    nav창 구성

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Main() {
   return (
-    <div>
-      <p>기본중에 기본</p>
-      <p>project 하나 만들기가 이렇게 힘이 듭니다</p>
+    <div class="navbar">
+        <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src={process.env.PUBLIC_URL+'/test.jpg'}></img>
+        </a>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link active" aria-current="page" href="#">미션</a>
+            <a class="nav-link" href="#">이용방법</a>
+            <a class="nav-link" href="#">못난이상점</a>
+            <a class="nav-link disabled">레시피</a>
+          </div>
+        </div>
+      </div>
+    </nav>
     </div>
   );
 }
 
-export default App;
+export default Main;

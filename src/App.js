6d9609fc70ac7468fb590cde60bdd37a5c3d076f
Yoon-TG <witteny@gamil.com>
2022-09-20 13:15:05 +0900

    sixthBanner 구성 완.

--- src/App.js
+++ src/App.js
@@ -43,19 +43,19 @@ function Main() {
           <div className='blankBig'></div>
           <div>
             <span className='leftText'>이렇게 많은 분들이</span>
-            <span className='rightText'>전체 만족도   </span>
-            <span className='rightText'>   전체 리뷰 수</span>
+            <span className='rightText'>전체 만족도&nbsp; &nbsp;</span>
+            <span className='rightText'>&nbsp;전체 리뷰 수</span>
             <br />
             <span className='leftText'>지속 가능한 식탁을 만들고 있어요!</span>
-            <span className='rightText'>4.8      </span>
-            <span className='rightText'>7257</span>
+            <span className='rightTextNum'>4.8&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+            <span className='rightTextNum'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;7257</span>
             <div className='blankSmall'></div>
           </div>
           <div className='secondBannerAni'>
             <img src='https://ik.imagekit.io/uhpparxr6/pc_review_thumb-2_PxTishY16.png?ik-sdk-version=react-1.1.1' />
           </div>
           <div className='blankSmall'></div>
-          <button className='secondBannerButton'>모든 리뷰 보기  →</button>
+          <button className='secondBannerButton'>모든 리뷰 보기&nbsp;&nbsp;&nbsp;→</button>
         </div>
         <div className='blankBig'></div>
 
@@ -73,58 +73,82 @@ function Main() {
         <div className='blankBig'></div>
   
         <div className='fifthBanner'>
-        <div className='fifthBannerImg' /> 
-        {/*배너 이미지 */}
-         {/* <div className='fifthBannerImg'> */}
-              {/* <img src='https://ik.imagekit.io/uhpparxr6/main-solution1.png?ik-sdk-version=react-1.1.1'></img> */}
-              {/* <img src='https://ik.imagekit.io/uhpparxr6/main-solution2.png?ik-sdk-version=react-1.1.1'></img>
-              <img src='https://ik.imagekit.io/uhpparxr6/main-solution3.png?ik-sdk-version=react-1.1.1'></img> */}
-            {/* </div> */}
+          <div className='fifthBannerImg' /> 
+          {/*배너 이미지 */}
+          {/* <div className='fifthBannerImg'> */}
+                {/* <img src='https://ik.imagekit.io/uhpparxr6/main-solution1.png?ik-sdk-version=react-1.1.1'></img> */}
+                {/* <img src='https://ik.imagekit.io/uhpparxr6/main-solution2.png?ik-sdk-version=react-1.1.1'></img>
+                <img src='https://ik.imagekit.io/uhpparxr6/main-solution3.png?ik-sdk-version=react-1.1.1'></img> */}
+              {/* </div> */}
 
-          <p>이렇게 해결해요</p>
-          <p style={{fontWeight:'bold', fontSize:'24px'}}>생산자와 소비자, 환경에도<br/>
-          더 이로운 방식으로
-          </p>
-          <div className='fifthBannerMain'>
-            <div>
-              
-              <p>
-                <div className='fifthBannerCircle'></div>
-                <span style={{fontWeight:'bold', fontSize:'24px', position:'relative', left:'10px', zIndex:'1'}}>1</span>
-                <span style={{fontWeight:'bold', position:'relative', left:'45px', marginBottom:'40px'}}>수확 직후 직매입</span><br/>
-                <span style={{position:'relative', left:'60px'}}>판로를 찾지 못한 농산물들을<br/>
-                농부님께 수확 2~7일 이내 직접 공급받아요.</span>
-              </p>
-              
+            <p>이렇게 해결해요</p>
+            <p style={{fontWeight:'bold', fontSize:'24px'}}>생산자와 소비자, 환경에도<br/>
+            더 이로운 방식으로
+            </p>
+            <div className='fifthBannerMain'>
+              <div>
+                <p>
+                  <div className='fifthBannerCircle'></div>
+                  <span style={{fontWeight:'bold', fontSize:'24px', position:'relative', left:'10px', zIndex:'1'}}>1</span>
+                  <span style={{fontWeight:'bold', position:'relative', left:'45px', marginBottom:'40px'}}>수확 직후 직매입</span><br/>
+                  <span style={{position:'relative', left:'60px'}}>판로를 찾지 못한 농산물들을<br/>
+                  농부님께 수확 2~7일 이내 직접 공급받아요.</span>
+                </p>
+                
+              </div>
+              <div>
+                <p>
+                  <div className='fifthBannerCircle'></div>
+                  <span style={{fontWeight:'bold', fontSize:'24px', position:'relative', left:'10px', zIndex:'1'}}>2</span>
+                  <span style={{fontWeight:'bold', position:'relative', left:'45px', marginBottom:'40px'}}>조금씩 다양하게 소포장</span><br/>
+                  <span style={{position:'relative', left:'60px'}}>
+                  가구 수에 맞게 소량으로 포장해요.</span>
+                </p>
+                {/* <p style={{fontWeight:'bold', fontSize:'24px'}}>2</p>
+                <p>조금씩 다양하게 소포장</p>
+                <p>가구 수에 맞게 소량으로 포장해요.</p> */}
+              </div>
+              <div>
+                  <div className='fifthBannerCircle'></div>
+                  <span style={{fontWeight:'bold', fontSize:'24px', position:'relative', left:'10px', zIndex:'1'}}>3</span>
+                  <span style={{fontWeight:'bold', position:'relative', left:'45px', marginBottom:'40px'}}>집 앞까지 정기배송</span><br/>
+                  <span style={{position:'relative', left:'60px'}}>
+                  중간 유통 절차없이 즉시 보내드려요.</span>
+                {/* <p style={{fontWeight:'bold', fontSize:'24px'}}>3</p>
+                <p>집 앞까지 정기배송</p>
+                <p>중간 유통 절차없이 즉시 보내드려요.</p> */}
+              </div>
+              <br />
+              <p style={{position:'relative', left:'500px', color:'#EB6440'}}>구독 신청하기></p>
             </div>
-            <div>
-              <p>
-                <div className='fifthBannerCircle'></div>
-                <span style={{fontWeight:'bold', fontSize:'24px', position:'relative', left:'10px', zIndex:'1'}}>2</span>
-                <span style={{fontWeight:'bold', position:'relative', left:'45px', marginBottom:'40px'}}>조금씩 다양하게 소포장</span><br/>
-                <span style={{position:'relative', left:'60px'}}>
-                가구 수에 맞게 소량으로 포장해요.</span>
-              </p>
-              {/* <p style={{fontWeight:'bold', fontSize:'24px'}}>2</p>
-              <p>조금씩 다양하게 소포장</p>
-              <p>가구 수에 맞게 소량으로 포장해요.</p> */}
-            </div>
-            <div>
-                <div className='fifthBannerCircle'></div>
-                <span style={{fontWeight:'bold', fontSize:'24px', position:'relative', left:'10px', zIndex:'1'}}>3</span>
-                <span style={{fontWeight:'bold', position:'relative', left:'45px', marginBottom:'40px'}}>집 앞까지 정기배송</span><br/>
-                <span style={{position:'relative', left:'60px'}}>
-                중간 유통 절차없이 즉시 보내드려요.</span>
-              {/* <p style={{fontWeight:'bold', fontSize:'24px'}}>3</p>
-              <p>집 앞까지 정기배송</p>
-              <p>중간 유통 절차없이 즉시 보내드려요.</p> */}
-            </div>
-            <br />
-            <p style={{position:'relative', left:'500px', color:'#EB6440'}}>구독 신청하기></p>
-          </div>
-          <div className='blankBig'/>
+            <div className='blankBig'/>
         </div>
 
+        <div className='sixthBanner'>
+          <div className='sixthBannerImg' />
+            <p>이렇게 보내드려요</p>
+            <p style={{fontWeight:'bold', fontSize:'24px'}}>가장 신선한 제철 채소를<br/>
+            소량씩 즐겨요
+            </p>
+            <div className='sixthBannerMain'>
+              <p>
+                <img src="https://ik.imagekit.io/uhpparxr6/main/Frame_4774_irxmm28Db.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1659941162686&ik-sdk-version=react-1.1.1"></img>
+                &nbsp; 7~10종 무농약·유기농 채소
+              </p>
+              <p>
+                <img src="https://ik.imagekit.io/uhpparxr6/main/Frame_4774_1__BUUTXzfH9.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1659941162743&ik-sdk-version=react-1.1.1"></img>
+                &nbsp; 원하지 않는 채소 제외
+              </p>
+              <p>
+                <img src="https://ik.imagekit.io/uhpparxr6/main/Frame_4774_2__XtGpKLkPU.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1659941162900&ik-sdk-version=react-1.1.1"></img>
+                &nbsp; 이번 주 구출 품목 별도 추가 
+              </p>
+            </div>
+        </div>
+        <div className='blankBig'/>
+        <div className='blankBig'/>
+        <div className='blankBig'/>
+        <button className='sixthBannerButton'>시작하기</button>
 
 
 
@@ -135,7 +159,6 @@ function Main() {
 
 
 
-        
     </div>
   );
 }
